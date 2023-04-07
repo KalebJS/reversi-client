4a807e70a9c02119833decc795bf81ca2d98ed04
Kaleb Smith <kalebsmith11@gmail.com>
2023-04-07 17:57:44 -0600

    fix: it works

--- reversi_bot.py
+++ reversi_bot.py
@@ -1,8 +1,22 @@
+from pathlib import Path
 import numpy as np
-from functools import cache
+from functools import cache, wraps
 from typing import Tuple, Union
 
 from state import ReversiGameState
+import pickle
+
+
+def dump_board(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except Exception as e:
+            with open("board.pkl", "wb") as f:
+                pickle.dump(args[0].board, f)
+            raise e
+    return wrapper
 
 
 class ReversiBot:
@@ -14,86 +28,65 @@ class ReversiBot:
 
     def discCount(self, state: ReversiGameState):
         # Get total discs for the current player
-        return np.sum(state.board == state.turn)
+        return np.sum(state.board == self.own_id)
 
     def discParity(self, state: ReversiGameState):
         # Get the difference in discs from the perspective of the current player (positive if more than enemy, negative if less than enemy)
-        return np.sum(state.board == state.turn) - np.sum(state.board == (3 - state.turn))
-
-    def availableMoves(self, state: ReversiGameState):
-        # How many moves can the current player make from this position (how many can the enemy make as well?)
-        ...
+        return np.sum(state.board == self.own_id) - np.sum(state.board == (3 - self.own_id))
 
     def cornersCaptured(self, state: ReversiGameState):
         # Get the number of corners captured by the current player
         totalCorners = 0
         corners = [(0,0),(0,7),(7,0),(7,7)]
         for corner in corners:
-            if state.board[corner[0]][corner[1]] == state.turn:
+            if state.board[corner[0]][corner[1]] == self.own_id:
                 totalCorners += 1
         return totalCorners
 
     def edgesCaptured(self, state: ReversiGameState):
         # Get the number of edges captured by the current player
-        player_mask = state.board == state.turn
-        row_mask = (1 << 8) - 1
-
-        top_row = player_mask[0]
-        bottom_row = player_mask[7]
-        left_col = player_mask[:, 0]
-        right_col = player_mask[:, 7]
+        player_mask = state.board == self.own_id
 
         total_edges = 0
 
         # Check top and bottom rows
-        if (top_row[1:-1] & ~row_mask[1:-1]).sum() == 0:
-            total_edges += 1
-        if (bottom_row[1:-1] & ~row_mask[1:-1]).sum() == 0:
+        if np.all(player_mask[0, :]) or np.all(player_mask[7, :]):
             total_edges += 1
 
         # Check left and right columns
-        left_col_mask = (1 << 56) | (1 << 48) | (1 << 40) | (1 << 32) | (1 << 24) | (1 << 16) | (1 << 8) | 1
-        right_col_mask = left_col_mask << 7
-
-        if (left_col[1:-1] & ~left_col_mask).sum() == 0:
-            total_edges += 1
-        if (right_col[1:-1] & ~right_col_mask).sum() == 0:
+        if np.all(player_mask[:, 0]) or np.all(player_mask[:, 7]):
             total_edges += 1
 
         return total_edges
 
     def fullEdgesCaptured(self, state: ReversiGameState):
         # Get the number of complete edges captured by the current player
-        player_mask = state.board == state.turn
-        row_mask = (1 << 8) - 1
+        player_board = np.array(state.board == self.own_id, dtype=np.int8)
 
-        top_row = player_mask[0]
-        bottom_row = player_mask[7]
-        left_col = player_mask[:, 0]
-        right_col = player_mask[:, 7]
+        top_row = player_board[0]
+        bottom_row = player_board[7]
+        left_col = player_board[:, 0]
+        right_col = player_board[:, 7]
 
         full_edges = 0
 
         # Check top and bottom rows
-        if (top_row & row_mask) == row_mask:
+        if np.all(top_row) or np.all(top_row[::-1]):
             full_edges += 1
-        if (bottom_row & row_mask) == row_mask:
+        if np.all(bottom_row) or np.all(bottom_row[::-1]):
             full_edges += 1
 
         # Check left and right columns
-        left_col_mask = (1 << 56) | (1 << 48) | (1 << 40) | (1 << 32) | (1 << 24) | (1 << 16) | (1 << 8) | 1
-        right_col_mask = left_col_mask << 7
-
-        if (left_col & left_col_mask) == left_col_mask:
+        if np.all(left_col) or np.all(left_col[::-1]):
             full_edges += 1
-        if (right_col & right_col_mask) == right_col_mask:
+        if np.all(right_col) or np.all(right_col[::-1]):
             full_edges += 1
 
         return full_edges
         
-    def permanantUnflippableDiscs(self, state: ReversiGameState):
+    def permanentUnflippableDiscs(self, state: ReversiGameState):
         # Get the number of unflippable discs
-        player_mask = state.board == state.turn
+        player_mask = state.board == self.own_id
 
         # Define masks for edge and corner squares
         corner_mask = np.zeros((8, 8), dtype=bool)
@@ -108,40 +101,40 @@ class ReversiBot:
 
         # Check corners
         if corner_mask[0, 0]:
-            if state.board[1, 0] == (3 - state.turn) and state.board[0, 1] == (3 - state.turn):
+            if state.board[1, 0] == (3 - self.own_id) and state.board[0, 1] == (3 - self.own_id):
                 unflippable_mask[0, 0] = True
 
         if corner_mask[0, 7]:
-            if state.board[0, 6] == (3 - state.turn) and state.board[1, 7] == (3 - state.turn):
+            if state.board[0, 6] == (3 - self.own_id) and state.board[1, 7] == (3 - self.own_id):
                 unflippable_mask[0, 7] = True
 
         if corner_mask[7, 0]:
-            if state.board[6, 0] == (3 - state.turn) and state.board[7, 1] == (3 - state.turn):
+            if state.board[6, 0] == (3 - self.own_id) and state.board[7, 1] == (3 - self.own_id):
                 unflippable_mask[7, 0] = True
 
         if corner_mask[7, 7]:
-            if state.board[6, 7] == (3 - state.turn) and state.board[7, 6] == (3 - state.turn):
+            if state.board[6, 7] == (3 - self.own_id) and state.board[7, 6] == (3 - self.own_id):
                 unflippable_mask[7, 7] = True
 
         # Check edges
         for i in range(1, 7):
             if edge_mask[0, i]:
-                if (state.board[0, i-1] == (3 - state.turn) and state.board[0, i+1] == (3 - state.turn) and
-                        state.board[1, i] == (3 - state.turn)):
+                if (state.board[0, i-1] == (3 - self.own_id) and state.board[0, i+1] == (3 - self.own_id) and
+                        state.board[1, i] == (3 - self.own_id)):
                     unflippable_mask[0, i] = True
 
             if edge_mask[7, i]:
-                if (state.board[7, i-1] == (3 - state.turn) and state.board[7, i+1] == (3 - state.turn) and
-                        state.board[6, i] == (3 - state.turn)):
+                if (state.board[7, i-1] == (3 - self.own_id) and state.board[7, i+1] == (3 - self.own_id) and
+                        state.board[6, i] == (3 - self.own_id)):
                     unflippable_mask[7, i] = True
 
             if edge_mask[i, 0]:
-                if (state.board[i-1, 0] == (3 - state.turn) and state.board[i+1, 0] == (3 - state.turn) and
-                    state.board[i, 1] == (3 - state.turn)):
+                if (state.board[i-1, 0] == (3 - self.own_id) and state.board[i+1, 0] == (3 - self.own_id) and
+                    state.board[i, 1] == (3 - self.own_id)):
                     unflippable_mask[i, 0] = True
                     if edge_mask[i, 7]:
-                        if (state.board[i-1, 7] == (3 - state.turn) and state.board[i+1, 7] == (3 - state.turn) and
-                                state.board[i, 6] == (3 - state.turn)):
+                        if (state.board[i-1, 7] == (3 - self.own_id) and state.board[i+1, 7] == (3 - self.own_id) and
+                                state.board[i, 6] == (3 - self.own_id)):
                             unflippable_mask[i, 7] = True
 
         # Count the number of unflippable discs
@@ -151,16 +144,16 @@ class ReversiBot:
 
     def tempUnflippableDiscs(self, state: ReversiGameState):
         # Define a mask for the current player's discs
-        player_mask = state.board == state.turn
+        player_mask = state.board == self.own_id
         
         # Define a mask for the opponent's discs
-        opp_mask = state.board == (3 - state.turn)
+        opp_mask = state.board == (3 - self.own_id)
         
         # Define a mask for empty squares
         empty_mask = (state.board == 0)
         
         # Get the set of legal moves for the current player
-        legal_moves = state.get_legal_actions()
+        legal_moves = state.get_valid_moves()
         
         # Define a mask for squares that can be temporarily unflippable
         unflippable_mask = np.zeros((8, 8), dtype=bool)
@@ -184,15 +177,17 @@ class ReversiBot:
                 # Check in each direction from the opponent's disc for an empty square
                 for drow, dcol in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
                     cur_row, cur_col = row + drow, col + dcol
+                    if cur_row < 0 or cur_row > 7 or cur_col < 0 or cur_col > 7:
+                        continue
                     cur_direction = np.zeros((8, 8), dtype=bool)
                     cur_direction[cur_row, cur_col] = True
                     
                     # Keep going in the same direction while we're on the board and we're on empty squares
                     while (cur_row >= 0 and cur_row <= 7 and cur_col >= 0 and cur_col <= 7 and 
                         empty_mask[cur_row, cur_col] and np.any(cur_direction & surround_mask)):
+                        cur_direction[cur_row, cur_col] = True
                         cur_row += drow
                         cur_col += dcol
-                        cur_direction[cur_row, cur_col] = True
                     
                     # If we've reached a player's disc, mark the squares between the opponent's disc and the player's disc as unflippable
                     if (cur_row >= 0 and cur_row <= 7 and cur_col >= 0 and cur_col <= 7 and 
@@ -205,14 +200,39 @@ class ReversiBot:
         return unflippable_discs
 
     def cornerDangerZones(self, state: ReversiGameState):
-        # Spaces that allow the enemy to capture corners (within 1 diagonal space of an unclaimed corner)
-        ...
+        dangerCoords = [(1,1), (1,6), (6,1), (6,6), (0,1), (1,0), (0,6), (1,7), (6,0), (7,1), (6,7), (7,6)]
+        corners = [(0,0), (0,7), (7,0), (7,7)]
+        dangerCount = 0
+        for corner in corners:
+            row, col = corner
+            if state.board[row][col] != 0: # Skip if corner is already occupied
+                continue
+            for coord in dangerCoords:
+                row_offset, col_offset = coord
+                danger_row = row + row_offset
+                danger_col = col + col_offset
+                if (0 <= danger_row < 8) and (0 <= danger_col < 8) and (state.board[danger_row][danger_col] == self.own_id):
+                    # Check if the disc in the danger zone is flippable
+                    flip_row = danger_row + row_offset
+                    flip_col = danger_col + col_offset
+                    while (0 <= flip_row < 8) and (0 <= flip_col < 8) and (state.board[flip_row][flip_col] == -self.own_id):
+                        flip_row += row_offset
+                        flip_col += col_offset
+                    if (0 <= flip_row < 8) and (0 <= flip_col < 8) and (state.board[flip_row][flip_col] == self.own_id):
+                        dangerCount += 1
+                elif (0 <= danger_row < 8) and (0 <= danger_col < 8) and (state.board[danger_row][danger_col] == -self.own_id):
+                    # Check if the disc in the danger zone is unflippable
+                    flip_row = danger_row + row_offset
+                    flip_col = danger_col + col_offset
+                    while (0 <= flip_row < 8) and (0 <= flip_col < 8) and (state.board[flip_row][flip_col] == -self.own_id):
+                        flip_row += row_offset
+                        flip_col += col_offset
+                    if (0 <= flip_row < 8) and (0 <= flip_col < 8) and (state.board[flip_row][flip_col] == self.own_id):
+                        pass
+                    else:
+                        dangerCount += 1
+        return dangerCount
 
-    def edgeDangerZones(self, state: ReversiGameState):
-        # Spaces that allow the enemy to capture edges (within 1 space of an unclaimed edge)
-        ...
-
-    
     @cache
     def heuristic(self, state: ReversiGameState):
         # Main heuristic function that calls the other various heuristic functions and applies multipliers to them
@@ -226,20 +246,82 @@ class ReversiBot:
                         \/         \/         \/               \/         \/         \/                     \/            \/        \/         \/         \/                                   \/             \/        \/          \/         \/         \/         \/                      \/         \/            \/         \/                   \/              \/                         \/             \/                   \/        \/                       \/          \/                                               \/             \/         \/     \/          \/ \/               \/             \/         \/       \_/             \/              \/             \/         \/             \/                   \/                        \/        \/ \/               \/         \/                      \/         \/                  \/         \/ )/            \/         \/                      \/         \/                \/                         \/        \/ \/               \/         \/                      \/         \/                  \/         \/  
         '''
         self.evaluations += 1
-        # moves_left = state.turns_remaining()
+        moves_left = state.turns_remaining()
 
-        # discs = self.discCount(state)
-        # parity = self.discParity(state)
+        discs = self.discCount(state)
+        parity = self.discParity(state)
+        corners = self.cornersCaptured(state)
+        edges = self.edgesCaptured(state)
+        sides = self.fullEdgesCaptured(state)
+        unflippable = self.permanentUnflippableDiscs(state)
+        tempUnflippable = self.tempUnflippableDiscs(state)
+        cornerDangerZone = self.cornerDangerZones(state)
         # mobility = self.availableMoves(state)
-        # corners = self.cornersCaptured(state)
-        # edges = self.edgesCaptured(state)
-        # sides = self.fullEdgesCaptured(state)
-        # unflippable = self.permanentUnflippableDiscs(state)
-        # tempUnflippable = self.tempUnflippableDiscs(state)
-        # riskySpace = self.riskySpace(state)
-        
-        return state.get_player_pieces(self.own_id)
+      
+        # Set initial weights
+        discs_weight = 0
+        parity_weight = 0
+        corners_weight = 0
+        edges_weight = 0
+        sides_weight = 0
+        unflippable_weight = 0
+        tempUnflippable_weight = 0
+        cornerDangerZone_weight = 0
+        # mobility_weight = 0
 
+        if moves_left <= 64:
+            discs_weight = 1
+            parity_weight = 1
+            corners_weight = 80
+            edges_weight = 50
+            sides_weight = 50
+            unflippable_weight = 8
+            tempUnflippable = 5
+            cornerDangerZone_weight = -15
+        if moves_left <= 50:
+            ...
+        if moves_left <= 40:
+            unflippable_weight = 50
+        if moves_left <= 30:
+            ...
+        if moves_left <= 20:
+            discs_weight = 45
+            tempUnflippable_weight = 8
+            corners_weight = 100
+        if moves_left <= 15:
+            ...
+        if moves_left <= 10:
+            discs_weight = 100
+            parity_weight = 30
+            corners_weight = 10
+            edges_weight = 10
+            sides_weight = 30
+            unflippable_weight = 15
+            tempUnflippable = 10
+            cornerDangerZone_weight = -5
+        if moves_left <= 5:
+            # discs_weight = .9
+            # parity_weight = .9
+            ...
+        if moves_left == 1:
+            discs_weight = 100
+            parity_weight = 0
+            corners_weight = 0
+            edges_weight = 0
+            sides_weight = 0
+            unflippable_weight = 0
+            tempUnflippable = 0
+            cornerDangerZone_weight = 0
+        
+        heuristicScore = (discs * discs_weight) + (parity * parity_weight) + (corners * corners_weight) + \
+                        (edges * edges_weight) + (sides * sides_weight) + (unflippable * unflippable_weight) + \
+                        (tempUnflippable * tempUnflippable_weight) + (cornerDangerZone * cornerDangerZone_weight)
+        
+        return heuristicScore
+        
+        # return state.get_player_pieces(self.own_id)
+
+    @dump_board
     def minimax(
         self,
         state: ReversiGameState,
@@ -264,6 +346,7 @@ class ReversiBot:
         """
 
         move_state = state.simulate_move(move)
+
         is_maximizing = move_state.turn == self.own_id
         if depth == 0 or not move_state.turns_remaining():
             return self.heuristic(move_state)
@@ -309,7 +392,7 @@ class ReversiBot:
         Move should be a tuple (row, col) of the move you want the bot to make.
         """
         valid_moves = state.get_valid_moves()
-        DEPTH = 5
+        DEPTH = 1
         best, res = float("-inf"), None
         for move in valid_moves:
             score = self.minimax(state, move, DEPTH, float("-inf"), float("inf"))
@@ -320,3 +403,15 @@ class ReversiBot:
 
         print(self.evaluations)
         return res 
+
+if __name__ == "__main__":
+    bot = ReversiBot(1)
+    # create 8x8 board numpy array
+    path = Path.cwd() / "board.pkl"
+    if not path.exists():
+        board = np.zeros((8, 8), dtype=int)
+    else:
+        with open("board.pkl", "rb") as f:
+            board = pickle.load(f)
+    state = ReversiGameState(board, 1)
+    bot.make_move(state)
